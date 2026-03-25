d38de6909e2a86ca53a5ec489013661355156672
sohith <sohith73@gmail.com>
2026-03-25 17:14:24 +0530

    Update exports.js

--- exports.js
+++ exports.js
@@ -1,6 +1,6 @@
 // API Configuration
-export const API_BASE_URL = 'http://localhost:5000';
-// export const API_BASE_URL = 'https://flashfire-backend-9wv0.onrender.com';
+// export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'https://flashfire-backend-9wv0.onrender.com';
 
 // API Endpoints
 export const API_ENDPOINTS = {
